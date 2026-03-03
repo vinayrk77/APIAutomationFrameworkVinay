e8794682ec8bc11bd3e91daba932e29387bd0daa
vinayrk77 <vinayrk26@gmail.com>
2026-03-03 17:40:57 +0530

    End to end Testing

--- src/test/java/com/vinaythetestingacademy/tests/restfulbooker/e2e_integration/TestIntegrationFlow1.java
+++ src/test/java/com/vinaythetestingacademy/tests/restfulbooker/e2e_integration/TestIntegrationFlow1.java
@@ -1,4 +1,132 @@
 package com.vinaythetestingacademy.tests.restfulbooker.e2e_integration;
 
-public class TestIntegrationFlow1 {
+import com.vinaythetestingacademy.base.BaseTest;
+import com.vinaythetestingacademy.endpoints.APIConstants;
+import com.vinaythetestingacademy.pojos.requestPOJO.Booking;
+import com.vinaythetestingacademy.pojos.responsePOJO.BookingResponse;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.restassured.RestAssured;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.requestSpecification;
+
+public class TestIntegrationFlow1 extends BaseTest {
+
+    // TestE2EFlow_01
+
+    //  Test E2E Scenario 1
+
+    //  1. Create a Booking -> bookingID
+
+    // 2. Create Token -> token
+
+    // 3. Verify that the Create Booking is working - GET Request to bookingID
+
+    // 4. Update the booking ( bookingID, Token) - Need to get the token, bookingID from above request
+
+    // 5. Delete the Booking - Need to get the token, bookingID from above request
+
+    @Test(groups = "qa", priority = 1)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 1. Verify that the Booking can be Created")
+    public void testCreateBooking(ITestContext iTestContext){
+
+        requestSpecification.basePath(APIConstants.CREATE_UPDATE_BOOKING_URL);
+        response = RestAssured.given(requestSpecification)
+                .when().body(payloadManager.createPayloadBookingAsString())
+                .post();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+        BookingResponse bookingResponse = payloadManager.bookingResponseJava(response.asString());
+        assertActions.verifyStringKey(bookingResponse.getBooking().getFirstname(), "Pramod");
+        assertActions.verifyStringKeyNotNull(bookingResponse.getBookingid());
+
+        Integer bookingid = bookingResponse.getBookingid();
+        iTestContext.setAttribute("bookingid",bookingid);
+
+
+
+    }
+
+    @Test(groups = "qa", priority = 2)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 2. Verify that the Booking By ID")
+    public void testVerifyBookingId(ITestContext iTestContext){
+        System.out.println(iTestContext.getAttribute("bookingid"));
+
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+
+        String basePathGET = APIConstants.CREATE_UPDATE_BOOKING_URL+"/" + bookingid;
+        System.out.println(basePathGET);
+
+        requestSpecification.basePath(basePathGET);
+        response = RestAssured
+                .given(requestSpecification)
+                .when().get();
+        validatableResponse = response.then().log().all();
+        // Validatable Assertion
+        validatableResponse.statusCode(200);
+
+        Booking booking = payloadManager.getResponseFromJSON(response.asString());
+        assertActions.verifyStringKeyNotNull(booking.getFirstname());
+
+
+
+
+
+    }
+
+    @Test(groups = "qa", priority = 3)
+    @Owner("Promode")
+    @Description("TC#INT1 - Step 3. Verify Updated Booking by ID")
+    public void testUpdateBookingByID(ITestContext iTestContext){
+
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+        String token = getToken();
+
+        iTestContext.setAttribute("token",token);
+
+
+        String basePathPUTPATCH = APIConstants.CREATE_UPDATE_BOOKING_URL + "/" + bookingid;
+        System.out.println(basePathPUTPATCH);
+
+        requestSpecification.basePath(basePathPUTPATCH);
+
+        response = RestAssured
+                .given(requestSpecification).cookie("token", token)
+                .when().body(payloadManager.fullUpdatePayloadAsString()).put();
+
+
+        validatableResponse = response.then().log().all();
+        // Validatable Assertion
+        validatableResponse.statusCode(200);
+
+        Booking booking = payloadManager.getResponseFromJSON(response.asString());
+
+        assertActions.verifyStringKeyNotNull(booking.getFirstname());
+        assertActions.verifyStringKey(booking.getFirstname(),"Lucky");
+
+
+    }
+
+    @Test(groups = "qa", priority = 4)
+    @Owner("Vinay")
+    @Description("TC#INT1 - Step 4. Delete the Booking by ID")
+    public void testDeleteBookingById(ITestContext iTestContext){
+
+        Integer bookingid = (Integer) iTestContext.getAttribute("bookingid");
+        String token = (String)iTestContext.getAttribute("token");
+
+        String basePathDELETE = APIConstants.CREATE_UPDATE_BOOKING_URL + "/" + bookingid;
+
+        requestSpecification.basePath(basePathDELETE).cookie("token", token);
+        validatableResponse = RestAssured.given().spec(requestSpecification)
+                .when().delete().then().log().all();
+        validatableResponse.statusCode(201);
+
+
+
+    }
 }
